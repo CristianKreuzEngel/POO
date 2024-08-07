d429ca05850a169bb359344c9395d7dec25482e6
Matheus Togni <matheustogni64@gmail.com>
2024-08-07 20:54:29 -0300

    começado

---
+++ Criptografia/Criptografia/Classes/AssimetricaAquivo.cs
@@ -0,0 +1,66 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class AsymmetricEncryption
+{
+    private RSA rsa;
+
+    public AsymmetricEncryption()
+    {
+        rsa = RSA.Create();
+    }
+
+    // Método para gerar e salvar chaves RSA
+    public void GenerateAndSaveKeys(string publicKeyPath, string privateKeyPath)
+    {
+        string publicKey = rsa.ToXmlString(false);  // Chave pública
+        string privateKey = rsa.ToXmlString(true);  // Chave privada
+
+        File.WriteAllText(publicKeyPath, publicKey);
+        File.WriteAllText(privateKeyPath, privateKey);
+
+        Console.WriteLine("Chaves geradas e salvas com sucesso.");
+    }
+
+    public void EncryptFile(string inputFilePath, string outputFilePath, string publicKey)
+    {
+        var data = File.ReadAllBytes(inputFilePath);
+        var encryptedData = EncryptData(data, publicKey);
+        File.WriteAllBytes(outputFilePath, encryptedData);
+    }
+
+    public void DecryptFile(string inputFilePath, string outputFilePath, string privateKey)
+    {
+        var encryptedData = File.ReadAllBytes(inputFilePath);
+        var decryptedData = DecryptData(encryptedData, privateKey);
+        File.WriteAllBytes(outputFilePath, decryptedData);
+    }
+
+    public string EncryptText(string text, string publicKey)
+    {
+        var data = Encoding.UTF8.GetBytes(text);
+        var encryptedData = EncryptData(data, publicKey);
+        return Convert.ToBase64String(encryptedData);
+    }
+
+    public string DecryptText(string encryptedText, string privateKey)
+    {
+        var encryptedData = Convert.FromBase64String(encryptedText);
+        var decryptedData = DecryptData(encryptedData, privateKey);
+        return Encoding.UTF8.GetString(decryptedData);
+    }
+
+    private byte[] EncryptData(byte[] data, string publicKey)
+    {
+        rsa.FromXmlString(publicKey);
+        return rsa.Encrypt(data, RSAEncryptionPadding.Pkcs1);
+    }
+
+    private byte[] DecryptData(byte[] data, string privateKey)
+    {
+        rsa.FromXmlString(privateKey);
+        return rsa.Decrypt(data, RSAEncryptionPadding.Pkcs1);
+    }
+}
--- Criptografia/Criptografia/Program.cs
+++ Criptografia/Criptografia/Program.cs
@@ -1,6 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.IO;
 
-using System;
-using Criptografia.Classes;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        AsymmetricEncryption encryption = new AsymmetricEncryption();
 
-Console.WriteLine("Hello, World!");
+        // Gerar e salvar chaves RSA
+        encryption.GenerateAndSaveKeys("publicKey.xml", "privateKey.xml");
+
+        // Ler as chaves dos arquivos
+        string publicKey = File.ReadAllText("publicKey.xml");
+        string privateKey = File.ReadAllText("privateKey.xml");
+
+        // Exemplo de criptografia e descriptografia de texto
+        string originalText = "Abacate";
+        string encryptedText = encryption.EncryptText(originalText, publicKey);
+        string decryptedText = encryption.DecryptText(encryptedText, privateKey);
+
+        Console.WriteLine("Original: " + originalText);
+        Console.WriteLine("Encrypted: " + encryptedText);
+        Console.WriteLine("Decrypted: " + decryptedText);
+
+        // Exemplo de criptografia e descriptografia de arquivos
+        string inputFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto.txt";
+        string encryptedFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto.txt";  // Substitua pelo caminho do arquivo criptografado
+        string decryptedFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto.txt";  // Substitua pelo caminho do arquivo descriptografado
+
+        encryption.EncryptFile(inputFilePath, encryptedFilePath, publicKey);
+        encryption.DecryptFile(encryptedFilePath, decryptedFilePath, privateKey);
+    }
+}
