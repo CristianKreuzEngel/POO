20577b3fd56671ce85d4a397a1d92ed9b5c12104
Matheus Togni <matheustogni64@gmail.com>
2024-08-07 21:41:05 -0300

    att

--- Criptografia/Criptografia/Program.cs
+++ Criptografia/Criptografia/Program.cs
@@ -64,13 +64,13 @@ public class Program
             {
                 case "1":
                     string inputFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto.txt";
-                    string encryptedFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto_criptografado.txt";
+                    string encryptedFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto.txt";
                     encryption.EncryptFile(inputFilePath, encryptedFilePath, publicKey);
                     Console.WriteLine("Arquivo criptografado com sucesso.");
                     break;
                 case "2":
-                    string encryptedInputFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto_criptografado.txt";
-                    string decryptedFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto_descriptografado.txt";
+                    string encryptedInputFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto.txt";
+                    string decryptedFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto.txt";
                     encryption.DecryptFile(encryptedInputFilePath, decryptedFilePath, privateKey);
                     Console.WriteLine("Arquivo descriptografado com sucesso.");
                     break;
