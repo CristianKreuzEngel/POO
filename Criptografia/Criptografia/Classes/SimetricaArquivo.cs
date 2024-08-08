c6b8427b965dbf77af93f30781c26977c316478e
Cristian Kreuz Engel <kreuzengelc@gmail.com>
2024-08-07 21:26:50 -0300

    Teste temporário

--- Criptografia/Criptografia/Classes/SimetricaArquivo.cs
+++ Criptografia/Criptografia/Classes/SimetricaArquivo.cs
@@ -1,10 +1,86 @@
 ﻿using System;
 using System.IO;
-using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Text;
 
-namespace Criptografia.Classes;
-
-public class SimetricaArquivo
+public class SymmetricEncryption
 {
-    
+    private readonly string key;
+    private readonly string iv;
+
+    public SymmetricEncryption()
+    {
+        // Gerar chave e IV aleatórios para criptografia simétrica
+        using (Aes aes = Aes.Create())
+        {
+            key = Convert.ToBase64String(aes.Key);
+            iv = Convert.ToBase64String(aes.IV);
+        }
+    }
+
+    public void EncryptFile(string inputFilePath, string outputFilePath)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Convert.FromBase64String(key);
+            aes.IV = Convert.FromBase64String(iv);
+
+            using (FileStream fsInput = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+            using (FileStream fsOutput = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write))
+            using (CryptoStream cs = new CryptoStream(fsOutput, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                fsInput.CopyTo(cs);
+            }
+        }
+    }
+
+    public void DecryptFile(string inputFilePath, string outputFilePath)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Convert.FromBase64String(key);
+            aes.IV = Convert.FromBase64String(iv);
+
+            using (FileStream fsInput = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+            using (FileStream fsOutput = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write))
+            using (CryptoStream cs = new CryptoStream(fsInput, aes.CreateDecryptor(), CryptoStreamMode.Read))
+            {
+                cs.CopyTo(fsOutput);
+            }
+        }
+    }
+
+    public string EncryptText(string plainText)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Convert.FromBase64String(key);
+            aes.IV = Convert.FromBase64String(iv);
+
+            using (MemoryStream ms = new MemoryStream())
+            using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            using (StreamWriter sw = new StreamWriter(cs))
+            {
+                sw.Write(plainText);
+            }
+
+            return Convert.ToBase64String(ms.ToArray());
+        }
+    }
+
+    public string DecryptText(string cipherText)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Convert.FromBase64String(key);
+            aes.IV = Convert.FromBase64String(iv);
+
+            using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(cipherText)))
+            using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+            using (StreamReader sr = new StreamReader(cs))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
 }
--- Criptografia/Criptografia/Program.cs
+++ Criptografia/Criptografia/Program.cs
@@ -6,6 +6,7 @@ public class Program
     public static void Main(string[] args)
     {
         AsymmetricEncryption asymmetricEncryption = new AsymmetricEncryption();
+        SymmetricEncryption symmetricEncryption = new SymmetricEncryption();
 
         // Gerar e salvar chaves RSA
         asymmetricEncryption.GenerateAndSaveKeys("publicKey.xml", "privateKey.xml");
@@ -21,7 +22,7 @@ public class Program
         {
             Console.WriteLine("Escolha o tipo de criptografia:");
             Console.WriteLine("1. Criptografia Assimétrica");
-            Console.WriteLine("2. Criptografia Simétrica (Não Implementado)");
+            Console.WriteLine("2. Criptografia Simétrica");
             Console.WriteLine("3. Sair");
             Console.Write("Opção: ");
             string choice = Console.ReadLine();
@@ -32,7 +33,7 @@ public class Program
                     ShowAsymmetricMenu(asymmetricEncryption, publicKey, privateKey, ref lastEncryptedText);
                     break;
                 case "2":
-                    Console.WriteLine("Criptografia Simétrica não está implementada.");
+                    ShowSymmetricMenu(symmetricEncryption, ref lastEncryptedText);
                     break;
                 case "3":
                     exit = true;
@@ -92,6 +93,67 @@ public class Program
                     else
                     {
                         string decryptedText = encryption.DecryptText(lastEncryptedText, privateKey);
+                        Console.WriteLine("Texto descriptografado: " + decryptedText);
+                    }
+                    break;
+                case "5":
+                    back = true;
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+
+    private static void ShowSymmetricMenu(SymmetricEncryption encryption, ref string lastEncryptedText)
+    {
+        bool back = false;
+
+        while (!back)
+        {
+            Console.WriteLine("Escolha a operação:");
+            Console.WriteLine("1. Criptografar um arquivo");
+            Console.WriteLine("2. Descriptografar um arquivo");
+            Console.WriteLine("3. Criptografar um texto");
+            Console.WriteLine("4. Descriptografar o texto criptografado");
+            Console.WriteLine("5. Voltar");
+            Console.Write("Opção: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    string inputFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto.txt";
+                    string encryptedFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto_criptografado_simetrico.txt";
+                    encryption.EncryptFile(inputFilePath, encryptedFilePath);
+                    Console.WriteLine("Arquivo criptografado com sucesso.");
+                    break;
+                case "2":
+                    string encryptedInputFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto_criptografado_simetrico.txt";
+                    string decryptedFilePath = "C:/POO ENGEL/POO/Criptografia/Criptografia/Arquivos/texto_descriptografado_simetrico.txt";
+                    encryption.DecryptFile(encryptedInputFilePath, decryptedFilePath);
+                    Console.WriteLine("Arquivo descriptografado com sucesso.");
+                    break;
+                case "3":
+                    string originalText = null;
+                    do
+                    {
+                        Console.Write("Digite o texto a ser criptografado (não pode ser vazio): ");
+                        originalText = Console.ReadLine();
+                    } while (string.IsNullOrWhiteSpace(originalText));
+
+                    lastEncryptedText = encryption.EncryptText(originalText);
+                    Console.WriteLine("Texto criptografado: " + lastEncryptedText);
+                    break;
+                case "4":
+                    if (lastEncryptedText == null)
+                    {
+                        Console.WriteLine("Nenhum texto criptografado encontrado.");
+                    }
+                    else
+                    {
+                        string decryptedText = encryption.DecryptText(lastEncryptedText);
                         Console.WriteLine("Texto descriptografado: " + decryptedText);
                     }
                     break;
